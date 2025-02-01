2f4ff73d2208d08cdc6ec70fc8f053d7d89bb3d9
adamou <adamsnikiema187@gmail.com>
2025-02-01 16:51:22 +0000

    2. Create Your Magical Tables

---
+++ MySQLServer.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+
+"CREATE DATABASE IF NOT EXISTS alx_book_store"
+
+
+def connect_database():
+    # Replace with your connection details
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="alx_book_store",
+    )
+    return mydb
