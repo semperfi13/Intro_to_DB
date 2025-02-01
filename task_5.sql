4973f1d230b83bc68dd0923b8f6fd58f2d2abcb0
adamou <adamsnikiema187@gmail.com>
2025-02-01 18:41:20 +0000

    5. Populating Your Database with Your Very First Data

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,8 @@
 USE alx_book_store;
 
 
-INSERT INTO `Customers` (customer_id,customer_name,email,address) 
-    VALUES(1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)  
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
 
 SELECT * FROM `Customers`
