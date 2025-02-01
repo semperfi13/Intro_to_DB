1825846ade47e751c964a86549447e39d7f9f6b7
adamou <adamsnikiema187@gmail.com>
2025-02-01 19:13:23 +0000

    1. Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,16 +1,23 @@
 import mysql.connector
 
 
-""" CREATE DATABASE IF NOT EXISTS alx_book_store
- """
+# Replace with your connection details
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="alx_book_store",
+)
 
 
-def connect_database():
-    # Replace with your connection details
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="alx_book_store",
-    )
-    return mydb
+mycursor = mydb.cursor()
+
+sql = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+mycursor.execute(sql)
+
+# Execute SQL statements using the execute() method on the cursor
+print("Database 'alx_book_store' created successfully! ")
+
+# Close connection to the databasse
+mycursor.close()
+mydb.close()
